03904f744a67f8232d6079bf3924b6053bf53934
sashmen5 <30698617+sashmen5@users.noreply.github.com>
2020-07-07 17:16:45 +0300

    removed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {apiInstance} from "./index";
 
-const accessId = "p-16daopsmc51f"; // String | Access ID
+const accessId = ""; // String | Access ID
 const opts = {
 };
 
